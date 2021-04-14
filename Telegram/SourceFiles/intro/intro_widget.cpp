1f6cd37169943413029763652550d8ddb32e8af2
John Preston <johnprestonmail@gmail.com>
2021-04-14 13:50:22 +0400

    Test linking with TDLib on Windows.

--- Telegram/SourceFiles/intro/intro_widget.cpp
+++ Telegram/SourceFiles/intro/intro_widget.cpp
@@ -46,6 +46,9 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "styles/style_intro.h"
 #include "base/qt/qt_common_adapters.h"
 
+#include "tdb/tdb_instance.h"
+#include "ui/toast/toast.h"
+
 namespace Intro {
 namespace {
 
@@ -64,6 +67,8 @@ using namespace ::Intro::details;
 	}
 	return Platform::SystemCountry();
 }
+
+std::unique_ptr<Tdb::Instance> Instance;
 
 } // namespace
 
@@ -91,6 +96,18 @@ Widget::Widget(
 		account,
 		rpl::single(true))) {
 	controller->setDefaultFloatPlayerDelegate(floatPlayerDelegate());
+
+	Instance = std::make_unique<Tdb::Instance>(Tdb::InstanceConfig{
+		.apiId = ApiId,
+		.apiHash = ApiHash,
+		.systemLanguageCode = Lang::GetInstance().systemLangCode(),
+		.deviceModel = Platform::DeviceModelPretty(),
+		.systemVersion = Platform::SystemVersionPretty(),
+		.applicationVersion = QString::fromLatin1(AppVersionStr),
+	});
+	Instance->testNetwork([](bool success) {
+		Ui::Toast::Show(u"TDLib Network "_q + (success ? "OK!" : "Fail :("));
+	});
 
 	getData()->country = ComputeNewAccountCountry();
 
@@ -856,6 +873,7 @@ void Widget::backRequested() {
 }
 
 Widget::~Widget() {
+	Instance = nullptr;
 	for (auto step : base::take(_stepHistory)) {
 		delete step;
 	}
---
+++ Telegram/SourceFiles/tdb/tdb_pch.h
@@ -0,0 +1,17 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#include <QtCore/QString>
+
+#include <range/v3/all.hpp>
+
+#include <rpl/rpl.h>
+#include <crl/crl.h>
+
+#include "base/basic_types.h"
+#include "base/flat_map.h"
+#include "base/flat_set.h"
