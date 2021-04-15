294a82d646cb4316e905171db4b9183e29e9f663
John Preston <johnprestonmail@gmail.com>
2021-04-15 12:17:24 +0400

    Use lib_tl codegen for TDLib API.

---
+++ Telegram/SourceFiles/tdb/details/tdb_tl_core_conversion_to.cpp
@@ -0,0 +1,32 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#include "tdb/details/tdb_tl_core_conversion_to.h"
+
+namespace Tdb {
+
+std::string tl_to_simple(const TLstring &value) {
+	return std::string(value.v.data(), value.v.size());
+}
+
+std::int32_t tl_to_simple(const TLint32 &value) {
+	return value.v;
+}
+
+std::int64_t tl_to_simple(const TLint64 &value) {
+	return value.v;
+}
+
+double tl_to_simple(const TLdouble &value) {
+	return value.v;
+}
+
+bool tl_to_simple(const TLbool &value) {
+	return (value.type() == id_boolTrue);
+}
+
+} // namespace Tdb
