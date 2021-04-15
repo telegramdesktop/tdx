deeff0b9e4d1183c170452b5f4b7d01a3eed6b96
John Preston <johnprestonmail@gmail.com>
2021-04-15 14:10:05 +0400

    Support nullable fields.

--- Telegram/SourceFiles/tdb/details/tdb_tl_core.h
+++ Telegram/SourceFiles/tdb/details/tdb_tl_core.h
@@ -10,6 +10,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "tl/tl_basic_types.h"
 #include "base/match_method.h"
 
+#include <optional>
+
 namespace td::tl {
 template <typename T>
 class unique_ptr;
@@ -22,6 +24,8 @@ class Function;
 
 namespace Tdb {
 
+inline constexpr auto null = std::nullopt;
+
 using TLint32 = tl::int_type;
 using TLint53 = tl::int64_type;
 using TLint64 = tl::int64_type;
@@ -31,16 +35,16 @@ using TLbytes = tl::bytes_type;
 template <typename T>
 using TLvector = tl::vector_type<T>;
 
-inline TLint32 tl_int32(int32 value) {
+inline constexpr TLint32 tl_int32(int32 value) noexcept {
 	return tl::make_int(value);
 }
-inline TLint53 tl_int53(int64 value) {
+inline constexpr TLint53 tl_int53(int64 value) noexcept {
 	return tl::make_int64(value);
 }
-inline TLint64 tl_int64(int64 value) {
+inline constexpr TLint64 tl_int64(int64 value) noexcept {
 	return tl::make_int64(value);
 }
-inline TLdouble tl_double(float64 value) {
+inline constexpr TLdouble tl_double(float64 value) noexcept {
 	return tl::make_double(value);
 }
 inline TLstring tl_string(const std::string &v) {
