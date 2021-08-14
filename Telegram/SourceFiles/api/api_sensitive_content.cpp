597d272b172710f0c5630da366cad227c3709353
23rd <23rd@vivaldi.net>
2021-08-15 00:28:02 +0300

    Migrated Api::SensitiveContent from MTP to TDLib.

--- Telegram/SourceFiles/api/api_sensitive_content.cpp
+++ Telegram/SourceFiles/api/api_sensitive_content.cpp
@@ -8,6 +8,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "api/api_sensitive_content.h"
 
 #include "apiwrap.h"
+#include "base/const_string.h"
+#include "tdb/tdb_option.h"
 #include "main/main_session.h"
 #include "main/main_account.h"
 #include "main/main_app_config.h"
@@ -15,16 +17,22 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 namespace Api {
 namespace {
 
+constexpr auto kSensitiveContentOption =
+	"ignore_sensitive_content_restrictions"_cs;
 constexpr auto kRefreshAppConfigTimeout = 3 * crl::time(1000);
 
 } // namespace
 
 SensitiveContent::SensitiveContent(not_null<ApiWrap*> api)
 : _session(&api->session())
+#if 0 // goodToRemove
 , _api(&api->instance())
+#endif
+, _api(&api->sender())
 , _appConfigReloadTimer([=] { _session->account().appConfig().refresh(); }) {
 }
 
+#if 0 // goodToRemove
 void SensitiveContent::reload() {
 	if (_requestId) {
 		return;
@@ -40,6 +48,25 @@ void SensitiveContent::reload() {
 		_requestId = 0;
 	}).send();
 }
+#endif
+
+void SensitiveContent::reload() {
+	using namespace Tdb;
+
+	const auto getOption = [&](
+			const QString &name,
+			not_null<rpl::variable<bool>*> variable) {
+		_api.request(TLgetOption(
+			tl_string(name)
+		)).done([=](const TLoptionValue &result) {
+			*variable = Tdb::OptionValue<bool>(result);
+		}).fail([](const Error &error) {
+		}).send();
+	};
+
+	getOption(kSensitiveContentOption.utf8(), &_enabled);
+	getOption(u"can_ignore_sensitive_content_restrictions"_q, &_canChange);
+}
 
 bool SensitiveContent::enabledCurrent() const {
 	return _enabled.current();
@@ -53,6 +80,7 @@ rpl::producer<bool> SensitiveContent::canChange() const {
 	return _canChange.value();
 }
 
+#if 0 // goodToRemove
 void SensitiveContent::update(bool enabled) {
 	if (!_canChange.current()) {
 		return;
@@ -66,6 +94,22 @@ void SensitiveContent::update(bool enabled) {
 	}).fail([=] {
 		_requestId = 0;
 	}).send();
+	_enabled = enabled;
+
+	_appConfigReloadTimer.callOnce(kRefreshAppConfigTimeout);
+}
+#endif
+
+void SensitiveContent::update(bool enabled) {
+	if (!_canChange.current()) {
+		return;
+	}
+	using namespace Tdb;
+	_api.request(TLsetOption(
+		tl_string(kSensitiveContentOption.utf8()),
+		tl_optionValueBoolean(tl_bool(enabled))
+	)).send();
+
 	_enabled = enabled;
 
 	_appConfigReloadTimer.callOnce(kRefreshAppConfigTimeout);
