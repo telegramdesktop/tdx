3e0774a477f78714951701a04de72c2aa459094e
23rd <23rd@vivaldi.net>
2021-08-19 09:48:43 +0300

    Migrated languages support from MTP to TDLib.
    This is only a partial migration with keeping the caching algorithm,
    it should be migrated too in the future.

--- Telegram/SourceFiles/api/api_updates.cpp
+++ Telegram/SourceFiles/api/api_updates.cpp
@@ -2603,6 +2603,7 @@ void Updates::feedUpdate(const MTPUpdate &update) {
 		});
 	} break;
 
+#if 0 // goodToRemove
 	////// Cloud langpacks
 	case mtpc_updateLangPack: {
 		const auto &data = update.c_updateLangPack();
@@ -2616,6 +2617,7 @@ void Updates::feedUpdate(const MTPUpdate &update) {
 			Lang::CurrentCloudManager().requestLangPackDifference(code);
 		}
 	} break;
+#endif
 
 	////// Cloud themes
 	case mtpc_updateTheme: {
@@ -2826,6 +2828,8 @@ void Updates::applyUpdate(const TLupdate &update) {
 	}, [&](const TLDupdateOption &data) {
 		if (session().api().globalPrivacy().apply(data)) {
 			return;
+		} else if (Lang::CurrentCloudManager().apply(data)) {
+			return;
 		}
 	}, [&](const TLDupdateStickerSet &data) {
 	}, [&](const TLDupdateInstalledStickerSets &data) {
@@ -2836,6 +2840,7 @@ void Updates::applyUpdate(const TLupdate &update) {
 	}, [&](const TLDupdateSelectedBackground &data) {
 	}, [&](const TLDupdateChatThemes &data) {
 	}, [&](const TLDupdateLanguagePackStrings &data) {
+		Lang::CurrentCloudManager().apply(data);
 	}, [&](const TLDupdateConnectionState &data) {
 	}, [&](const TLDupdateTermsOfService &data) {
 	}, [&](const TLDupdateUsersNearby &data) {
