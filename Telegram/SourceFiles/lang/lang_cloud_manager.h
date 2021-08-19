d012e7f3809d306472bde1100b98625a6c1bc8ff
23rd <23rd@vivaldi.net>
2021-08-19 09:48:43 +0300

    Migrated languages support from MTP to TDLib.
    This is only a partial migration with keeping the caching algorithm,
    it should be migrated too in the future.

--- Telegram/SourceFiles/lang/lang_cloud_manager.h
+++ Telegram/SourceFiles/lang/lang_cloud_manager.h
@@ -20,7 +20,10 @@ class Instance;
 enum class Pack;
 struct Language;
 
+#if 0 // goodToRemove
 Language ParseLanguage(const MTPLangPackLanguage &data);
+#endif
+Language ParseLanguage(const Tdb::TLlanguagePackInfo &result);
 
 class CloudManager : public base::has_weak_ptr {
 public:
@@ -34,9 +37,14 @@ public:
 	}
 	[[nodiscard]] rpl::producer<> languageListChanged() const;
 	[[nodiscard]] rpl::producer<> firstLanguageSuggestion() const;
+	void requestLangPackStrings(const QString &langId);
+#if 0 // goodToRemove
 	void requestLangPackDifference(const QString &langId);
 	void applyLangPackDifference(const MTPLangPackDifference &difference);
 	void setCurrentVersions(int version, int baseVersion);
+#endif
+	void apply(const Tdb::TLDupdateLanguagePackStrings &result);
+	bool apply(const Tdb::TLDupdateOption &result);
 
 	void resetToDefault();
 	void switchWithWarning(const QString &id);
@@ -49,10 +57,18 @@ public:
 	}
 
 private:
+	void init();
 	mtpRequestId &packRequestId(Pack pack);
 	mtpRequestId packRequestId(Pack pack) const;
 	Pack packTypeFromId(const QString &id) const;
+	void requestLangPackStrings(Pack pack);
+#if 0 // goodToRemove
 	void requestLangPackDifference(Pack pack);
+#endif
+	void applyStrings(
+		Pack pack,
+		// Instance::TLStrings.
+		const Tdb::TLvector<Tdb::TLlanguagePackString> &strings);
 	bool canApplyWithoutRestart(const QString &id) const;
 	void performSwitchToCustom();
 	void performSwitch(const Language &data);
@@ -64,7 +80,9 @@ private:
 	Language findOfferedLanguage() const;
 
 	void requestLanguageAndSwitch(const QString &id, bool warning);
+#if 0 // goodToRemove
 	void applyLangPackData(Pack pack, const MTPDlangPackDifference &data);
+#endif
 	void switchLangPackId(const Language &data);
 	void changeIdAndReInitConnection(const Language &data);
 
