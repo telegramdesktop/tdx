ce1b35aa1bc8822c8a367186575ab7c17722959e
23rd <23rd@vivaldi.net>
2021-10-20 08:11:24 +0300

    Added helper class to generate TDLib files from raw bytes.

--- Telegram/cmake/td_tdb.cmake
+++ Telegram/cmake/td_tdb.cmake
@@ -17,6 +17,8 @@ nice_target_sources(td_tdb ${src_loc}
 PRIVATE
     tdb/tdb_account.cpp
     tdb/tdb_account.h
+    tdb/tdb_file_generator.cpp
+    tdb/tdb_file_generator.h
     tdb/tdb_file_proxy.cpp
     tdb/tdb_file_proxy.h
     tdb/tdb_request_id.h
