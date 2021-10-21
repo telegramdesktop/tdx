0baa44791f05fb6c820fee15080b45a643effb93
John Preston <johnprestonmail@gmail.com>
2021-10-21 16:20:18 +0400

    Implement streamed loader using proxy file.

--- Telegram/SourceFiles/data/stickers/data_stickers_set.h
+++ Telegram/SourceFiles/data/stickers/data_stickers_set.h
@@ -119,6 +119,7 @@ private:
 	const not_null<Data::Session*> _owner;
 
 	CloudFile _thumbnail;
+	uint64 _tdbThumbnailRemoteLocationHash = 0;
 	std::weak_ptr<StickersSetThumbnailView> _thumbnailView;
 
 };
