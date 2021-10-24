8dff7301b071b0f052ba4e17d9f6346ee5852879
23rd <23rd@vivaldi.net>
2021-10-24 22:14:47 +0300

    Migrated group calls from MTP to TDLib.
    Notes.
    - There is a server bug: a participant of a Live Stream after
    raising their hand and allowing them to speak, disappears from a list.
    - groupCallRecentSpeakers are used only in Ui::GroupCallBar.
    - "canBeUnmutedFor*" are currently unused.
    - Since TDLib has no video_joined info we set videoIsWorking always true
    just like the Android client does.

--- Telegram/SourceFiles/data/data_types.h
+++ Telegram/SourceFiles/data/data_types.h
@@ -134,7 +134,10 @@ using WebPageId = uint64;
 using GameId = uint64;
 using PollId = uint64;
 using WallPaperId = uint64;
+#if 0 // goodToRemove
 using CallId = uint64;
+#endif
+using CallId = int;
 using BotAppId = uint64;
 
 constexpr auto CancelledWebPageId = WebPageId(0xFFFFFFFFFFFFFFFFULL);
