13893f8d64bcbce081a2300cdec64714149ca128
John Preston <johnprestonmail@gmail.com>
2021-11-08 09:58:59 +0400

    Improve text messages sending.

--- Telegram/SourceFiles/history/history_item_helpers.cpp
+++ Telegram/SourceFiles/history/history_item_helpers.cpp
@@ -96,6 +96,8 @@ MessageFlags FlagsFromTdb(const TLDmessage &data) {
 		return false;
 	});
 	return sendingOrFailedFlag
+		| (int(sendingOrFailedFlag) ? Flag::Local : Flag())
+		| (data.vscheduling_state() ? Flag() : Flag::HistoryEntry)
 		| (data.vis_outgoing().v ? Flag::Outgoing : Flag())
 		| (data.vcontains_unread_mention().v ? Flag::MentionsMe : Flag())
 		| (mediaUnread ? Flag::MediaIsUnread : Flag())
