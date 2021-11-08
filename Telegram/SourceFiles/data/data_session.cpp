f43f7c3e406796819e2d60e10e2c90480e615b71
John Preston <johnprestonmail@gmail.com>
2021-11-08 12:15:44 +0400

    !chat.last_message doesn't mean empty history.

--- Telegram/SourceFiles/data/data_session.cpp
+++ Telegram/SourceFiles/data/data_session.cpp
@@ -1219,8 +1219,6 @@ not_null<PeerData*> Session::processPeer(const TLchat &dialog) {
 	}
 	if (const auto message = data.vlast_message()) {
 		history->applyLastMessage(*message);
-	} else {
-		history->clearLastMessage();
 	}
 	history->applyDialogFields(
 		history->folder(),
