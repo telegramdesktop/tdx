12528402b2c008603b7bd3c6d32ea065c9452bb9
John Preston <johnprestonmail@gmail.com>
2021-11-08 12:07:59 +0400

    Don't display connecting from MTP state.

--- Telegram/SourceFiles/window/window_connecting_widget.cpp
+++ Telegram/SourceFiles/window/window_connecting_widget.cpp
@@ -308,6 +308,7 @@ void ConnectionState::refreshState() {
 		const auto ready = (Checker().state() == Checker::State::Ready);
 		const auto state = _account->mtp().dcstate();
 		const auto proxy = Core::App().settings().proxy().isEnabled();
+#if 0 // todo
 		if (state == MTP::ConnectingState
 			|| state == MTP::DisconnectedState
 			|| (state < 0 && state > -600)) {
@@ -320,6 +321,7 @@ void ConnectionState::refreshState() {
 			const auto wait = ((-state) / 1000) + 1;
 			return { State::Type::Waiting, proxy, exposed, under, ready, wait };
 		}
+#endif
 		return { State::Type::Connected, proxy, exposed, under, ready };
 	}();
 	if (state.exposed && state.waitTillRetry > 0) {
