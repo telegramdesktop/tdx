d4776ecfc3b89e226fcacda60719f382158e5bf9
23rd <23rd@vivaldi.net>
2021-11-16 05:39:48 +0300

    Migrated PeerListGlobalSearchController from MTP to TDLib.

--- Telegram/SourceFiles/boxes/peer_list_controllers.h
+++ Telegram/SourceFiles/boxes/peer_list_controllers.h
@@ -12,6 +12,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "base/weak_ptr.h"
 #include "base/timer.h"
 
+#include "tdb/tdb_sender.h"
+
 class History;
 
 namespace Data {
@@ -76,14 +78,23 @@ public:
 private:
 	bool searchInCache();
 	void searchOnServer();
+#if 0 // goodToRemove
 	void searchDone(const MTPcontacts_Found &result, mtpRequestId requestId);
+#endif
+	void searchDone(const Tdb::TLusers &result, mtpRequestId requestId);
 
 	const not_null<Main::Session*> _session;
+#if 0 // goodToRemove
 	MTP::Sender _api;
+#endif
+	Tdb::Sender _api;
 	base::Timer _timer;
 	QString _query;
 	mtpRequestId _requestId = 0;
+#if 0 // goodToRemove
 	std::map<QString, MTPcontacts_Found> _cache;
+#endif
+	std::map<QString, Tdb::TLusers> _cache;
 	std::map<mtpRequestId, QString> _queries;
 
 };
