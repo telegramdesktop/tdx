adbc98a834fce4d7aa9c163941df3f5c61c5d6f3
John Preston <johnprestonmail@gmail.com>
2021-12-01 17:23:09 +0400

    Update TDLib revision.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1904,7 +1904,7 @@ win:
 stage('td', """
     git clone https://github.com/tdlib/td.git
     cd td
-    git checkout 505fed00bd
+    git checkout 1d3cf2c209
 win:
     SET OPENSSL_DIR=%LIBS_DIR%\\openssl
     SET OPENSSL_LIBS_DIR=%OPENSSL_DIR%\\out
