a4aa07fa8c5b4de79e308dc8082cc37a666c126e
John Preston <johnprestonmail@gmail.com>
2021-12-02 10:08:38 +0400

    Request QR code only once.

--- Telegram/SourceFiles/intro/intro_qr.cpp
+++ Telegram/SourceFiles/intro/intro_qr.cpp
@@ -240,11 +240,7 @@ void QrWidget::checkForTokenUpdate(const MTPUpdate &update) {
 bool QrWidget::applyState(const TLauthorizationState &state) {
 	return state.match([&](
 			const TLDauthorizationStateWaitPhoneNumber &data) {
-		api().request(TLrequestQrCodeAuthentication(
-			tl_vector<TLint53>(0)
-		)).fail([=](const Error &error) {
-			showTokenError(error);
-		}).send();
+		sendRequestCode();
 		return true;
 	}, [&](const TLDauthorizationStateWaitOtherDeviceConfirmation &data) {
 		_qrLinks.fire_copy(data.vlink().v);
@@ -252,6 +248,18 @@ bool QrWidget::applyState(const TLauthorizationState &state) {
 	}, [&](const auto &) {
 		return false;
 	});
+}
+
+void QrWidget::sendRequestCode() {
+	if (_requestId) {
+		return;
+	}
+	_requestId = api().request(TLrequestQrCodeAuthentication(
+		tl_vector<TLint53>(0)
+	)).fail([=](const Error &error) {
+		_requestId = 0;
+		showTokenError(error);
+	}).send();
 }
 
 void QrWidget::submit() {
