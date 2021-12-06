1de2f9d8d608f4f952fbbd8d924c0ece83c1d481
John Preston <johnprestonmail@gmail.com>
2021-12-06 17:14:49 +0400

    Mark mentions as read.

--- Telegram/SourceFiles/history/history_item.cpp
+++ Telegram/SourceFiles/history/history_item.cpp
@@ -1252,6 +1252,7 @@ bool HistoryItem::isIncomingUnreadMedia() const {
 void HistoryItem::markMediaAndMentionRead() {
 	_flags &= ~MessageFlag::MediaIsUnread;
 
+#if 0 // mtp
 	if (mentionsMe()) {
 		_history->updateChatListEntry();
 		_history->unreadMentions().erase(id);
@@ -1273,6 +1274,7 @@ void HistoryItem::markMediaAndMentionRead() {
 			}
 		}
 	}
+#endif
 }
 
 void HistoryItem::markReactionsRead() {
