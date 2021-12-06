5f9aeb29ee94e0260007312a60b96a8f1e9c7007
John Preston <johnprestonmail@gmail.com>
2021-12-06 15:26:13 +0400

    Invoke viewMessages in all chats for all messages.

--- Telegram/SourceFiles/api/api_views.h
+++ Telegram/SourceFiles/api/api_views.h
@@ -7,7 +7,9 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
+#if 0 // mtp
 #include "mtproto/sender.h"
+#endif
 #include "base/timer.h"
 
 class ApiWrap;
@@ -44,14 +46,18 @@ private:
 			not_null<PeerData*>,
 			QVector<MTPint>> &prepared);
 
+#if 0 // mtp
 	void done(
 		QVector<MTPint> ids,
 		const MTPmessages_MessageViews &result,
 		mtpRequestId requestId);
 	void fail(const MTP::Error &error, mtpRequestId requestId);
+#endif
 
 	const not_null<Main::Session*> _session;
+#if 0 // mtp
 	MTP::Sender _api;
+#endif
 
 	base::flat_map<not_null<PeerData*>, base::flat_set<MsgId>> _incremented;
 	base::flat_map<not_null<PeerData*>, base::flat_set<MsgId>> _toIncrement;
