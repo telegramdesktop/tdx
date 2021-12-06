aabe69055929dd40c9306fabe6fd08f866837173
John Preston <johnprestonmail@gmail.com>
2021-12-06 15:26:13 +0400

    Invoke viewMessages in all chats for all messages.

--- Telegram/SourceFiles/history/history_inner_widget.cpp
+++ Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -1170,7 +1170,8 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 			} else if (isUnread) {
 				readTill = item;
 			}
-			if (markingAsViewed && item->hasViews()) {
+			// Always inform TDLib about messages being viewed.
+			if (markingAsViewed && item->isRegular()) {
 				session().api().views().scheduleIncrement(item);
 			}
 			if (withReaction) {
