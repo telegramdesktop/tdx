6210c2eb3f610f7d9e5f79e69f794a2087a88e05
John Preston <johnprestonmail@gmail.com>
2022-02-04 16:33:35 +0300

    Track message reactions. Cut out some code.

--- Telegram/SourceFiles/history/history_inner_widget.cpp
+++ Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -1182,10 +1182,12 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 				_widget->enqueueMessageHighlight(view, {});
 			}
 		}
+#if 0 // mtp
 		session().data().reactions().poll(item, context.now);
 		if (item->hasExtendedMediaPreview()) {
 			session().api().views().pollExtendedMedia(item);
 		}
+#endif
 		_reactionsManager->recordCurrentReactionEffect(
 			item->fullId(),
 			QPoint(0, top));
