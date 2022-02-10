740dad32310573042b17c381b5c3da612904418f
John Preston <johnprestonmail@gmail.com>
2022-02-10 16:10:04 +0300

    Migrate favorite reaction read / write to TDLib.

--- Telegram/SourceFiles/data/data_message_reactions.cpp
+++ Telegram/SourceFiles/data/data_message_reactions.cpp
@@ -40,6 +40,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "base/random.h"
 
 #include "tdb/tdb_tl_scheme.h"
+#include "tdb/tdb_account.h"
 
 namespace Data {
 namespace {
@@ -466,6 +467,7 @@ const Reaction *Reactions::favorite() const {
 }
 
 void Reactions::setFavorite(const ReactionId &id) {
+#if 0 // mtp
 	const auto api = &_owner->session().api();
 	if (_saveFaveRequestId) {
 		api->request(_saveFaveRequestId).cancel();
@@ -477,6 +479,11 @@ void Reactions::setFavorite(const ReactionId &id) {
 	}).fail([=] {
 		_saveFaveRequestId = 0;
 	}).send();
+#endif
+	_owner->session().sender().request(TLsetOption(
+		tl_string("default_reaction"),
+		tl_optionValueString(tl_string(emoji))
+	)).send();
 
 	applyFavorite(id);
 }
