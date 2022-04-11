c821260e47fa87cfb1627287c8a9be4d5dfb7eac
John Preston <johnprestonmail@gmail.com>
2022-04-11 09:53:29 +0400

    Fix build after rebase.

--- Telegram/SourceFiles/inline_bots/bot_attach_web_view.cpp
+++ Telegram/SourceFiles/inline_bots/bot_attach_web_view.cpp
@@ -654,6 +654,7 @@ void AttachWebView::botSendData(QByteArray data) {
 		return;
 	}
 	const auto randomId = base::RandomValue<uint64>();
+#if 0 // todo
 	_session->api().request(MTPmessages_SendWebViewData(
 		_bot->inputUser,
 		MTP_long(randomId),
@@ -662,6 +663,7 @@ void AttachWebView::botSendData(QByteArray data) {
 	)).done([=](const MTPUpdates &result) {
 		_session->api().applyUpdates(result);
 	}).send();
+#endif
 	crl::on_main(this, [=] { cancel(); });
 }
 
@@ -1486,6 +1488,7 @@ void AttachWebView::started(uint64 queryId) {
 	base::timer_each(
 		kProlongTimeout
 	) | rpl::start_with_next([=] {
+#if 0 // todo
 		using Flag = MTPmessages_ProlongWebView::Flag;
 		_session->api().request(base::take(_prolongId)).cancel();
 		_prolongId = _session->api().request(MTPmessages_ProlongWebView(
@@ -1503,6 +1506,7 @@ void AttachWebView::started(uint64 queryId) {
 		)).done([=] {
 			_prolongId = 0;
 		}).send();
+#endif
 	}, _panel->lifetime());
 }
 
