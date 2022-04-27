5a3eb723f89818c79f29e36663ece78459809f7c
John Preston <johnprestonmail@gmail.com>
2022-04-27 20:24:09 +0400

    Migrate scheduled messages to TDLib.

--- Telegram/SourceFiles/apiwrap.cpp
+++ Telegram/SourceFiles/apiwrap.cpp
@@ -557,6 +557,8 @@ void ApiWrap::sendMessageFail(
 		uint64 randomId,
 		FullMsgId itemId) {
 	const auto show = ShowForPeer(peer);
+
+#if 0 // mtp
 	if (show && error == u"PEER_FLOOD"_q) {
 		show->showBox(
 			Ui::MakeInformBox(
@@ -615,6 +617,7 @@ void ApiWrap::sendMessageFail(
 			}
 		}
 	}
+#endif
 }
 
 void ApiWrap::requestMessageData(
