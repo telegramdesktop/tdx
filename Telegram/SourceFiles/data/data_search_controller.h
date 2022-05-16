d39fc21ae92dd098df236f3f1ba4b4adcc5e66dd
23rd <23rd@vivaldi.net>
2022-05-16 23:21:27 +0300

    Migrated shared media from MTP to TDLib.

--- Telegram/SourceFiles/data/data_search_controller.h
+++ Telegram/SourceFiles/data/data_search_controller.h
@@ -17,6 +17,13 @@ namespace Main {
 class Session;
 } // namespace Main
 
+namespace Tdb {
+class TLmessages;
+class TLsearchChatMessages;
+class TLgetChatHistory;
+class TLmessages;
+} // namespace Tdb
+
 namespace Data {
 enum class LoadDirection : char;
 } // namespace Data
@@ -29,14 +36,21 @@ struct SearchResult {
 	int fullCount = 0;
 };
 
+#if 0 // goodToRemove
 using SearchRequest = MTPmessages_Search;
 using SearchRequestResult = MTPmessages_Messages;
+#endif
+using SearchRequest = Tdb::TLsearchChatMessages;
+using SearchRequestResult = Tdb::TLmessages;
 
 using HistoryResult = SearchResult;
+#if 0 // mtp
 using HistoryRequest = MTPmessages_GetHistory;
 using HistoryRequestResult = MTPmessages_Messages;
+#endif
+using HistoryRequest = Tdb::TLgetChatHistory;
+using HistoryRequestResult = Tdb::TLmessages;
 
-#if 0 // mtp
 [[nodiscard]] std::optional<SearchRequest> PrepareSearchRequest(
 	not_null<PeerData*> peer,
 	MsgId topicRootId,
@@ -51,7 +65,6 @@ using HistoryRequestResult = MTPmessages_Messages;
 	MsgId messageId,
 	Data::LoadDirection direction,
 	const SearchRequestResult &data);
-#endif
 
 [[nodiscard]] HistoryRequest PrepareHistoryRequest(
 	not_null<PeerData*> peer,
