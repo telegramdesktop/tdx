4a960ad1090049b193576a117c084e07e22ec091
John Preston <johnprestonmail@gmail.com>
2022-07-05 19:23:48 +0400

    Migrate deleteChatHistory to TDLib.

--- Telegram/SourceFiles/apiwrap.cpp
+++ Telegram/SourceFiles/apiwrap.cpp
@@ -2217,7 +2217,12 @@ void ApiWrap::clearHistory(not_null<PeerData*> peer, bool revoke) {
 }
 
 void ApiWrap::deleteConversation(not_null<PeerData*> peer, bool revoke) {
-#if 0 // todo
+	sender().request(TLdeleteChatHistory(
+		peerToTdbChat(peer->id),
+		tl_bool(true),
+		tl_bool(revoke)
+	)).send();
+#if 0 // mtp
 	if (const auto chat = peer->asChat()) {
 		request(MTPmessages_DeleteChatUser(
 			MTP_flags(0),
