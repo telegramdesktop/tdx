8ad2c1442eadcfd7ce50bd869e1c87fb7545e1d4
John Preston <johnprestonmail@gmail.com>
2022-07-20 17:22:09 +0300

    Fix flickering online members count.

--- Telegram/SourceFiles/api/api_chat_participants.cpp
+++ Telegram/SourceFiles/api/api_chat_participants.cpp
@@ -491,7 +491,7 @@ void ChatParticipants::requestBots(not_null<ChannelData*> channel) {
 	)).done([=](const Tdb::TLDchatMembers &data) {
 		_botsRequests.remove(channel);
 		const auto &[availableCount, list] = Parse(channel, data);
-		ApplyLastList(channel, availableCount, list);
+		ApplyBotsList(channel, availableCount, list);
 	}).fail([=] {
 		_botsRequests.remove(channel);
 	}).send();
