352e4365a5c1a1ffd4934f933d9cfc2abe59067c
John Preston <johnprestonmail@gmail.com>
2022-08-05 14:17:49 +0300

    Implement text / caption edit using TDLib.

--- Telegram/SourceFiles/api/api_editing.cpp
+++ Telegram/SourceFiles/api/api_editing.cpp
@@ -20,9 +20,12 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "mtproto/mtproto_response.h"
 #include "boxes/abstract_box.h" // Ui::show().
 
+#include "tdb/tdb_sender.h"
+
 namespace Api {
 namespace {
 
+using namespace Tdb;
 using namespace rpl::details;
 
 template <typename T>
@@ -219,7 +222,23 @@ mtpRequestId EditCaption(
 		SendOptions options,
 		Fn<void()> done,
 		Fn<void(const QString &)> fail) {
+	const auto session = &item->history()->session();
+	return session->sender().request(TLeditMessageCaption(
+		peerToTdbChat(item->history()->peer->id),
+		tl_int53(item->id.bare),
+		Api::FormattedTextToTdb(
+			session,
+			caption,
+			Api::ConvertOption::SkipLocal)
+	)).done([=](const TLmessage &result) {
+		session->data().processMessage(result, NewMessageType::Existing);
+		done();
+	}).fail([=](const Error &error) {
+		fail(error.message);
+	}).send();
+#if 0 // mtp
 	return EditMessage(item, caption, options, done, fail);
+#endif
 }
 
 mtpRequestId EditTextMessage(
@@ -228,11 +247,30 @@ mtpRequestId EditTextMessage(
 		SendOptions options,
 		Fn<void(mtpRequestId requestId)> done,
 		Fn<void(const QString &, mtpRequestId requestId)> fail) {
+	const auto session = &item->history()->session();
+	return session->sender().request(TLeditMessageText(
+		peerToTdbChat(item->history()->peer->id),
+		tl_int53(item->id.bare),
+		tl_inputMessageText(
+			Api::FormattedTextToTdb(
+				session,
+				caption,
+				Api::ConvertOption::SkipLocal),
+			tl_bool(options.removeWebPageId),
+			tl_bool(true))
+	)).done([=](const TLmessage &result, RequestId requestId) {
+		session->data().processMessage(result, NewMessageType::Existing);
+		done(requestId);
+	}).fail([=](const Error &error, RequestId requestId) {
+		fail(error.message, requestId);
+	}).send();
+#if 0 // mtp
 	const auto callback = [=](Fn<void()> applyUpdates, mtpRequestId id) {
 		applyUpdates();
 		done(id);
 	};
 	return EditMessage(item, caption, options, callback, fail);
+#endif
 }
 
 } // namespace Api
