a54cd23006cfb404bc9335c3d7f147f030ba1e75
John Preston <johnprestonmail@gmail.com>
2022-12-23 18:51:46 +0400

    Send message thread loading requests.

--- Telegram/SourceFiles/window/window_peer_menu.cpp
+++ Telegram/SourceFiles/window/window_peer_menu.cpp
@@ -467,7 +467,7 @@ void TogglePinnedThread(
 			owner->notifyPinnedDialogsOrderUpdated();
 		}).send();
 	} else if (const auto topic = thread->asTopic()) {
-		// todo
+		// later-todo pinned topics
 	}
 }
 
