f7e7a584f2dae313b1a915ba10422a930aa1dcd0
John Preston <johnprestonmail@gmail.com>
2022-12-26 16:33:22 +0400

    Migrate audio transcribe to TDLib.

--- Telegram/SourceFiles/api/api_transcribes.cpp
+++ Telegram/SourceFiles/api/api_transcribes.cpp
@@ -15,14 +15,21 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_peer.h"
 #include "apiwrap.h"
 
+#include "tdb/tdb_tl_scheme.h"
+
 namespace Api {
+namespace {
+
+using namespace Tdb;
+
+} // namespace
 
 Transcribes::Transcribes(not_null<ApiWrap*> api)
 : _session(&api->session())
-#if 0 // todo
+#if 0 // mtp
 , _api(&api->instance()) {
 #endif
-{
+, _api(&api->sender()) {
 }
 
 void Transcribes::toggle(not_null<HistoryItem*> item) {
@@ -34,7 +41,10 @@ void Transcribes::toggle(not_null<HistoryItem*> item) {
 		_session->data().requestItemResize(item);
 	} else if (!i->second.requestId) {
 		i->second.shown = !i->second.shown;
+#if 0 // mtp
 		if (i->second.roundview) {
+#endif
+		if (i->second.roundview && !i->second.pending) {
 			_session->data().requestItemViewRefresh(item);
 		}
 		_session->data().requestItemResize(item);
@@ -48,7 +58,7 @@ const Transcribes::Entry &Transcribes::entry(
 	return (i != _map.end()) ? i->second : empty;
 }
 
-#if 0 // todo
+#if 0 // mtp
 void Transcribes::apply(const MTPDupdateTranscribedAudio &update) {
 	const auto id = update.vtranscription_id().v;
 	const auto i = _ids.find(id);
@@ -71,6 +81,33 @@ void Transcribes::apply(const MTPDupdateTranscribedAudio &update) {
 }
 #endif
 
+void Transcribes::apply(
+		not_null<HistoryItem*> item,
+		const TLspeechRecognitionResult &result,
+		bool roundview) {
+	auto &entry = _map[item->fullId()];
+	entry.roundview = roundview;
+	result.match([&](const TLDspeechRecognitionResultText &result) {
+		entry.requestId = 0;
+		entry.result = result.vtext().v;
+		entry.pending = false;
+	}, [&](const TLDspeechRecognitionResultPending &result) {
+		entry.result = result.vpartial_text().v;
+		entry.pending = true;
+	}, [&](const TLDspeechRecognitionResultError &result) {
+		entry.requestId = 0;
+		entry.pending = false;
+		entry.failed = true;
+		if (result.verror().data().vmessage().v == u"MSG_VOICE_TOO_LONG"_q) {
+			entry.toolong = true;
+		}
+	});
+	if (entry.roundview && !entry.pending) {
+		_session->data().requestItemViewRefresh(item);
+	}
+	_session->data().requestItemResize(item);
+}
+
 void Transcribes::load(not_null<HistoryItem*> item) {
 	if (!item->isHistoryEntry() || item->isLocal()) {
 		return;
@@ -86,8 +123,22 @@ void Transcribes::load(not_null<HistoryItem*> item) {
 		}
 	};
 	const auto id = item->fullId();
-	const auto requestId = 0;
-#if 0 // todo
+	const auto requestId = _api.request(TLrecognizeSpeech(
+		peerToTdbChat(item->history()->peer->id),
+		tl_int53(item->id.bare)
+	)).done([=] {
+		auto &entry = _map[id];
+		if (entry.requestId) {
+			entry.requestId = 0;
+			if (const auto item = _session->data().message(id)) {
+				if (!entry.pending) {
+					toggleRound(item, entry);
+				}
+				_session->data().requestItemResize(item);
+			}
+		}
+	}).fail([=](const Error &error) {
+#if 0 // mtp
 	const auto requestId = _api.request(MTPmessages_TranscribeAudio(
 		item->history()->peer->input,
 		MTP_int(item->id)
@@ -103,11 +154,15 @@ void Transcribes::load(not_null<HistoryItem*> item) {
 			_session->data().requestItemResize(item);
 		}
 	}).fail([=](const MTP::Error &error) {
+#endif
 		auto &entry = _map[id];
 		entry.requestId = 0;
 		entry.pending = false;
 		entry.failed = true;
+#if 0 // mtp
 		if (error.type() == u"MSG_VOICE_TOO_LONG"_q) {
+#endif
+		if (error.message == u"MSG_VOICE_TOO_LONG"_q) {
 			entry.toolong = true;
 		}
 		if (const auto item = _session->data().message(id)) {
@@ -115,12 +170,14 @@ void Transcribes::load(not_null<HistoryItem*> item) {
 			_session->data().requestItemResize(item);
 		}
 	}).send();
-#endif
 	auto &entry = _map.emplace(id).first->second;
 	entry.requestId = requestId;
 	entry.shown = true;
 	entry.failed = false;
+#if 0 // mtp
 	entry.pending = false;
+#endif
+	entry.pending = true;
 }
 
 } // namespace Api
