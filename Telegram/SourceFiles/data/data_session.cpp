6eb829adf234a62bfbba8099a1d7a72a0f96666c
John Preston <johnprestonmail@gmail.com>
2022-12-27 16:50:31 +0400

    Migrate unavailable reasons to TDLib.

--- Telegram/SourceFiles/data/data_session.cpp
+++ Telegram/SourceFiles/data/data_session.cpp
@@ -257,6 +257,7 @@ void CheckForSwitchInlineButton(not_null<HistoryItem*> item) {
 			0.,
 			double(std::numeric_limits<int>::max())));
 }
+#endif
 
 } // namespace
 
@@ -1177,7 +1178,8 @@ not_null<UserData*> Session::processUser(const TLuser &user) {
 	} else {
 		result->clearPhoto();
 	}
-	result->setUnavailableReasons({}); // todo
+	result->setUnavailableReasons(
+		ExtractUnavailableReasons(data.vrestriction_reason()));
 
 	data.vtype().match([&](const TLDuserTypeRegular &) {
 		result->setFlags(result->flags() & ~UserDataFlag::Deleted);
@@ -1315,7 +1317,7 @@ not_null<PeerData*> Session::processPeer(const TLchat &dialog) {
 			chat->setGroupCallDefaultJoinAs(as);
 		}
 		const auto setFlags = Flag::CallNotEmpty | Flag::NoForwards;
-		chat->setFlags((chat->flags() & ~setFlags) // todo ?
+		chat->setFlags((chat->flags() & ~setFlags)
 			| (videoChat.vhas_participants().v
 				? Flag::CallNotEmpty
 				: Flag())
@@ -1363,7 +1365,7 @@ not_null<PeerData*> Session::processPeer(const TLchat &dialog) {
 			channel->setGroupCallDefaultJoinAs(as);
 		}
 		const auto setFlags = Flag::CallNotEmpty | Flag::NoForwards;
-		channel->setFlags((channel->flags() & ~setFlags) // todo ?
+		channel->setFlags((channel->flags() & ~setFlags)
 			| (videoChat.vhas_participants().v
 				? Flag::CallNotEmpty
 				: Flag())
@@ -1412,10 +1414,8 @@ not_null<ChatData*> Session::processChat(const TLbasicGroup &chat) {
 		result->setAdminRights(data.vis_anonymous().v
 			? ChatAdminRight::Anonymous
 			: ChatAdminRight());
-		//data.vcustom_title().v; // todo
 	}, [&](const TLDchatMemberStatusAdministrator &data) {
 		result->setAdminRights(AdminRightsFromChatMemberStatus(data));
-		//data.vcustom_title().v; // todo
 	}, [&](const TLDchatMemberStatusMember &data) {
 		result->setAdminRights(ChatAdminRights());
 	}, [&](const TLDchatMemberStatusRestricted &data) {
@@ -1481,7 +1481,10 @@ not_null<ChannelData*> Session::processChannel(
 			? Flag::Creator
 			: Flag())
 		| (data.vis_forum().v ? Flag::Forum : Flag());
-	//data.vrestriction_reason(); // todo
+
+	result->setUnavailableReasons(
+		ExtractUnavailableReasons(data.vrestriction_reason()));
+
 	data.vstatus().match([&](const TLDchatMemberStatusCreator &data) {
 		if (!data.vis_member().v) {
 			flags |= Flag::Left;
@@ -1490,7 +1493,7 @@ not_null<ChannelData*> Session::processChannel(
 			? ChatAdminRight::Anonymous
 			: ChatAdminRight());
 		result->setRestrictions(ChatRestrictionsInfo());
-		//data.vcustom_title().v; // todo
+		// custom_title is not set here, so ignored.
 	}, [&](const TLDchatMemberStatusAdministrator &data) {
 		using Flag = ChatAdminRight;
 		const auto bit = [&](const TLbool &check, Flag value) {
@@ -1498,7 +1501,7 @@ not_null<ChannelData*> Session::processChannel(
 		};
 		result->setAdminRights(AdminRightsFromChatMemberStatus(data));
 		result->setRestrictions(ChatRestrictionsInfo());
-		//data.vcustom_title().v; // todo
+		// custom_title is not set here, so ignored.
 	}, [&](const TLDchatMemberStatusMember &data) {
 		result->setAdminRights(ChatAdminRights());
 		result->setRestrictions(ChatRestrictionsInfo());
