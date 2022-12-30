b3af5bab1ea28ab1d0a6c545899e8e9bef939697
John Preston <johnprestonmail@gmail.com>
2022-12-30 21:09:45 +0400

    Wait for chat filters after logging in.

--- Telegram/SourceFiles/intro/intro_widget.cpp
+++ Telegram/SourceFiles/intro/intro_widget.cpp
@@ -273,6 +273,11 @@ void Widget::handleUpdate(const MTPUpdate &update) {
 void Widget::handleUpdate(const TLupdate &update) {
 	update.match([&](const TLDupdateAuthorizationState &data) {
 		handleAuthorizationState(data.vauthorization_state());
+	}, [&](const TLDupdateChatFilters &data) {
+		if (getData()->waitingForFilters) {
+			getData()->filtersUpdate = std::make_shared<TLupdate>(update);
+			getStep()->filtersReceived();
+		}
 	}, [](const auto &) {
 	});
 }
@@ -296,6 +301,7 @@ void Widget::handleAuthorizationState(const TLauthorizationState &state) {
 			= data.vrecovery_email_address_pattern().v;
 		getData()->pwdState.notEmptyPassport = data.vhas_passport_data().v;
 	}, [&](const TLDauthorizationStateReady &) {
+		getData()->waitingForFilters = true;
 	}, [&](const TLDauthorizationStateLoggingOut &) {
 	}, [&](const TLDauthorizationStateClosing &) {
 	}, [&](const TLDauthorizationStateClosed &) {
