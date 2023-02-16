e147351774fdc950ebacbf59570b956b9475b683
John Preston <johnprestonmail@gmail.com>
2023-02-16 18:32:08 +0400

    Correctly support animated single-emoji messages.

--- Telegram/SourceFiles/history/history_item.h
+++ Telegram/SourceFiles/history/history_item.h
@@ -21,6 +21,7 @@ class TLDmessageForwardInfo;
 class TLmessageInteractionInfo;
 class TLmessageReaction;
 class TLunreadReaction;
+class TLDmessageAnimatedEmoji;
 } // namespace Tdb
 
 class HiddenSenderInfo;
@@ -681,6 +682,7 @@ private:
 		const QVector<Tdb::TLunreadReaction> &list);
 	void setMedia(const Tdb::TLmessageContent &content);
 	void setContent(const Tdb::TLmessageContent &content);
+	void setAnimatedEmojiText(const Tdb::TLDmessageAnimatedEmoji &data);
 #if 0 // mtp
 	static void FillForwardedInfo(
 		CreateConfig &config,
