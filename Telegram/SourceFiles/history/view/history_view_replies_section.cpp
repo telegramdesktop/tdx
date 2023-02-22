3c018f721f590921bfd3ba8fc125a1c38cfd8efa
John Preston <johnprestonmail@gmail.com>
2023-02-22 16:49:21 +0400

    Just remove some more todo-s.

--- Telegram/SourceFiles/history/view/history_view_replies_section.cpp
+++ Telegram/SourceFiles/history/view/history_view_replies_section.cpp
@@ -1245,14 +1245,14 @@ void RepliesWidget::edit(
 		return;
 	}
 
+#if 0 // mtp
 	lifetime().add([=] {
 		if (!*saveEditMsgRequestId) {
 			return;
 		}
-#if 0 // todo
 		session().api().request(base::take(*saveEditMsgRequestId)).cancel();
-#endif
 	});
+#endif
 
 	const auto done = [=](mtpRequestId requestId) {
 		if (requestId == *saveEditMsgRequestId) {
--- Telegram/SourceFiles/history/view/history_view_scheduled_section.cpp
+++ Telegram/SourceFiles/history/view/history_view_scheduled_section.cpp
@@ -668,14 +668,14 @@ void ScheduledWidget::edit(
 		return;
 	}
 
+#if 0 // mtp
 	lifetime().add([=] {
 		if (!*saveEditMsgRequestId) {
 			return;
 		}
-#if 0 // todo
 		session().api().request(base::take(*saveEditMsgRequestId)).cancel();
-#endif
 	});
+#endif
 
 	const auto done = [=](mtpRequestId requestId) {
 		if (requestId == *saveEditMsgRequestId) {
