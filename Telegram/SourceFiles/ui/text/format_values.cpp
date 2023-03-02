767b3d144a09e895163eafe91192cb9cb4f2e7c1
John Preston <johnprestonmail@gmail.com>
2023-03-02 23:02:01 +0400

    Pause/unpause all Tdb while short animation plays.

--- Telegram/SourceFiles/ui/text/format_values.cpp
+++ Telegram/SourceFiles/ui/text/format_values.cpp
@@ -381,7 +381,7 @@ QString FormatImageSizeText(const QSize &size) {
 		+ QString::number(size.height());
 }
 
-#if 0 // doLater Don't let to use Ui::FormatPhone for now.
+#if 0 // mtp Don't let to use Ui::FormatPhone for now.
 QString FormatPhone(const QString &phone) {
 	if (phone.isEmpty()) {
 		return QString();
