54093bed70884b8f6d23d91a2ca0e14f79353885
John Preston <johnprestonmail@gmail.com>
2023-03-04 16:45:44 +0400

    Support collages of media in webpage previews.

--- Telegram/SourceFiles/data/data_session.cpp
+++ Telegram/SourceFiles/data/data_session.cpp
@@ -4387,8 +4387,10 @@ void Session::webpageApplyFields(
 		storyId,
 		photo,
 		document,
+#if 0 // mtp
 		std::move(collage),
 		std::move(iv),
+#endif
 		std::move(stickerSet),
 		duration,
 		author,
--- Telegram/SourceFiles/data/data_web_page.cpp
+++ Telegram/SourceFiles/data/data_web_page.cpp
@@ -21,6 +21,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "tdb/tdb_tl_scheme.h"
 #include "api/api_text_entities.h"
 
+#include "data/stickers/data_stickers.h"
+
 #include <xxhash.h>
 
 namespace {
@@ -126,8 +128,367 @@ WebPageCollage ExtractCollage(
 }
 #endif
 
+WebPageCollage ExtractCollage(
+		not_null<Data::Session*> owner,
+		const QVector<TLpageBlock> &items) {
+	const auto count = items.size();
+	if (count < 2) {
+		return {};
+	}
+	const auto bad = ranges::find_if(items, [](uint32 type) {
+		return (type != id_pageBlockPhoto) && (type != id_pageBlockVideo);
+	}, &TLpageBlock::type);
+	if (bad != items.end()) {
+		return {};
+	}
+
+	auto result = WebPageCollage();
+	result.items.reserve(count);
+	for (const auto &item : items) {
+		const auto good = item.match([&](const TLDpageBlockPhoto &data) {
+			if (const auto found = data.vphoto()) {
+				const auto photo = owner->processPhoto(*found);
+				if (photo->isNull()) {
+					return false;
+				}
+				result.items.emplace_back(photo);
+				return true;
+			}
+			return false;
+		}, [&](const TLDpageBlockVideo &data) {
+			if (const auto found = data.vvideo()) {
+				const auto document = owner->processDocument(*found);
+				if (!document->isVideoFile()) {
+					return false;
+				}
+				result.items.emplace_back(document);
+				return true;
+			}
+			return false;
+		}, [](const auto &) -> bool {
+			Unexpected("Type of block in Collage.");
+		});
+		if (!good) {
+			return {};
+		}
+	}
+	return result;
+}
+
+WebPageCollage ExtractCollage(
+		not_null<Data::Session*> owner,
+		const TLDwebPageInstantView &data) {
+	for (const auto &block : data.vpage_blocks().v) {
+		switch (block.type()) {
+		case id_pageBlockPhoto:
+		case id_pageBlockVideo:
+		case id_pageBlockCover:
+		case id_pageBlockEmbedded:
+		case id_pageBlockEmbeddedPost:
+		case id_pageBlockAudio:
+			return WebPageCollage();
+		case id_pageBlockSlideshow:
+			return ExtractCollage(
+				owner,
+				block.c_pageBlockSlideshow().vpage_blocks().v);
+		case id_pageBlockCollage:
+			return ExtractCollage(
+				owner,
+				block.c_pageBlockCollage().vpage_blocks().v);
+		default: break;
+		}
+	}
+	return WebPageCollage();
+}
+
+WebPageType ParseWebPageType(const TLlinkPreviewType &type, bool hasIV) {
+	const auto article = hasIV
+		? WebPageType::ArticleWithIV
+		: WebPageType::Article;
+	return type.match([&](const TLDlinkPreviewTypeAlbum &data) {
+		return WebPageType::Album;
+	}, [&](const TLDlinkPreviewTypeAnimation &data) {
+		return WebPageType::Video;
+	}, [&](const TLDlinkPreviewTypeApp &data) {
+		return article;
+	}, [&](const TLDlinkPreviewTypeArticle &data) {
+		return article;
+	}, [&](const TLDlinkPreviewTypeAudio &data) {
+		return WebPageType::Document;
+	}, [&](const TLDlinkPreviewTypeBackground &data) {
+		return WebPageType::WallPaper;
+	}, [&](const TLDlinkPreviewTypeChannelBoost &data) {
+		return WebPageType::ChannelBoost;
+	}, [&](const TLDlinkPreviewTypeChat &data) {
+		return data.vtype().match([&](const TLDinviteLinkChatTypeChannel &) {
+			return data.vcreates_join_request().v
+				? WebPageType::ChannelWithRequest
+				: WebPageType::Channel;
+		}, [&](const auto &) {
+			return data.vcreates_join_request().v
+				? WebPageType::GroupWithRequest
+				: WebPageType::Group;
+		});
+	}, [&](const TLDlinkPreviewTypeDocument &data) {
+		return WebPageType::Document;
+	}, [&](const TLDlinkPreviewTypeEmbeddedAnimationPlayer &data) {
+		return WebPageType::Video;
+	}, [&](const TLDlinkPreviewTypeEmbeddedAudioPlayer &data) {
+		return article;
+	}, [&](const TLDlinkPreviewTypeEmbeddedVideoPlayer &data) {
+		return WebPageType::Video;
+	}, [&](const TLDlinkPreviewTypeExternalAudio &data) {
+		return article;
+	}, [&](const TLDlinkPreviewTypeExternalVideo &data) {
+		return WebPageType::Video;
+	}, [&](const TLDlinkPreviewTypeInvoice &data) {
+		return article;
+	}, [&](const TLDlinkPreviewTypeMessage &data) {
+		return WebPageType::Message;
+	}, [&](const TLDlinkPreviewTypePhoto &data) {
+		return WebPageType::Photo;
+	}, [&](const TLDlinkPreviewTypePremiumGiftCode &data) {
+		return WebPageType::Giftcode;
+	}, [&](const TLDlinkPreviewTypeShareableChatFolder &data) {
+		return article;
+	}, [&](const TLDlinkPreviewTypeSticker &data) {
+		return WebPageType::Document;
+	}, [&](const TLDlinkPreviewTypeStickerSet &data) {
+		return WebPageType::StickerSet;
+	}, [&](const TLDlinkPreviewTypeStory &data) {
+		return WebPageType::Story;
+	}, [&](const TLDlinkPreviewTypeSupergroupBoost &data) {
+		return WebPageType::GroupBoost;
+	}, [&](const TLDlinkPreviewTypeTheme &data) {
+		return WebPageType::Theme;
+	}, [&](const TLDlinkPreviewTypeUnsupported &data) {
+		return article;
+	}, [&](const TLDlinkPreviewTypeUser &data) {
+		return data.vis_bot().v ? WebPageType::Bot : WebPageType::User;
+	}, [&](const TLDlinkPreviewTypeVideo &data) {
+		return WebPageType::Video;
+	}, [&](const TLDlinkPreviewTypeVideoChat &data) {
+		return data.vis_live_stream().v
+			? WebPageType::Livestream
+			: WebPageType::VoiceChat;
+	}, [&](const TLDlinkPreviewTypeVideoNote &data) {
+		return WebPageType::Document;
+	}, [&](const TLDlinkPreviewTypeVoiceNote &data) {
+		return WebPageType::Document;
+	}, [&](const TLDlinkPreviewTypeWebApp &data) {
+		return WebPageType::BotApp;
+	});
+}
+
+[[nodiscard]] FullStoryId ExtractStoryId(const TLlinkPreviewType &type) {
+	return type.match([&](const TLDlinkPreviewTypeStory &data) {
+		return FullStoryId{
+			peerFromTdbChat(data.vstory_sender_chat_id()),
+			data.vstory_id().v,
+		};
+	}, [&](const auto &) {
+		return FullStoryId();
+	});
+}
+
+[[nodiscard]] PhotoData *ExtractPhoto(
+		not_null<Data::Session*> owner,
+		const TLlinkPreviewType &type) {
+	const auto null = (PhotoData*)nullptr;
+	const auto photo = [&](const auto &from) {
+		using Type = std::decay_t<decltype(from.vphoto())>;
+		if constexpr (std::is_same_v<Type, TLphoto>) {
+			return owner->processPhoto(from.vphoto()).get();
+		} else {
+			return from.vphoto()
+				? owner->processPhoto(*from.vphoto()).get()
+				: nullptr;
+		}
+	};
+	return type.match([&](const TLDlinkPreviewTypeAlbum &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeAnimation &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeApp &data) {
+		return photo(data);
+	}, [&](const TLDlinkPreviewTypeArticle &data) {
+		return photo(data);
+	}, [&](const TLDlinkPreviewTypeAudio &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeBackground &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeChannelBoost &data) {
+		return photo(data);
+	}, [&](const TLDlinkPreviewTypeChat &data) {
+		return photo(data);
+	}, [&](const TLDlinkPreviewTypeDocument &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeEmbeddedAnimationPlayer &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeEmbeddedAudioPlayer &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeEmbeddedVideoPlayer &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeExternalAudio &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeExternalVideo &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeInvoice &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeMessage &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypePhoto &data) {
+		return photo(data);
+	}, [&](const TLDlinkPreviewTypePremiumGiftCode &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeShareableChatFolder &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeSticker &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeStickerSet &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeStory &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeSupergroupBoost &data) {
+		return photo(data);
+	}, [&](const TLDlinkPreviewTypeTheme &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeUnsupported &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeUser &data) {
+		return photo(data);
+	}, [&](const TLDlinkPreviewTypeVideo &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeVideoChat &data) {
+		return photo(data);
+	}, [&](const TLDlinkPreviewTypeVideoNote &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeVoiceNote &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeWebApp &data) {
+		return photo(data);
+	});
+}
+
+[[nodiscard]] DocumentData *ExtractDocument(
+		not_null<Data::Session*> owner,
+		const TLlinkPreviewType &type) {
+	const auto null = (DocumentData*)nullptr;
+	const auto document = [&](const auto &document) {
+		return owner->processDocument(document).get();
+	};
+	return type.match([&](const TLDlinkPreviewTypeAlbum &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeAnimation &data) {
+		return document(data.vanimation());
+	}, [&](const TLDlinkPreviewTypeApp &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeArticle &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeAudio &data) {
+		return document(data.vaudio());
+	}, [&](const TLDlinkPreviewTypeBackground &data) {
+		return data.vdocument() ? document(*data.vdocument()) : null;
+	}, [&](const TLDlinkPreviewTypeChannelBoost &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeChat &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeDocument &data) {
+		return document(data.vdocument());
+	}, [&](const TLDlinkPreviewTypeEmbeddedAnimationPlayer &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeEmbeddedAudioPlayer &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeEmbeddedVideoPlayer &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeExternalAudio &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeExternalVideo &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeInvoice &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeMessage &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypePhoto &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypePremiumGiftCode &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeShareableChatFolder &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeSticker &data) {
+		return document(data.vsticker());
+	}, [&](const TLDlinkPreviewTypeStickerSet &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeStory &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeSupergroupBoost &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeTheme &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeUnsupported &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeUser &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeVideo &data) {
+		return document(data.vvideo());
+	}, [&](const TLDlinkPreviewTypeVideoChat &data) {
+		return null;
+	}, [&](const TLDlinkPreviewTypeVideoNote &data) {
+		return document(data.vvideo_note());
+	}, [&](const TLDlinkPreviewTypeVoiceNote &data) {
+		return document(data.vvoice_note());
+	}, [&](const TLDlinkPreviewTypeWebApp &data) {
+		return null;
+	});
+}
+
+[[nodiscard]] std::unique_ptr<WebPageStickerSet> ExtractStickerSet(
+		not_null<Data::Session*> owner,
+		const TLlinkPreviewType &type) {
+	return type.match([&](const TLDlinkPreviewTypeStickerSet &data) {
+		auto items = std::vector<not_null<DocumentData*>>();
+		for (const auto &sticker : data.vstickers().v) {
+			if (const auto document = owner->processDocument(sticker)) {
+				if (document->sticker()) {
+					items.push_back(document);
+				}
+			}
+		}
+		if (!items.empty()) {
+			auto result = std::make_unique<WebPageStickerSet>();
+			const auto info = items.front()->sticker();
+			result->isEmoji = info->setType == Data::StickersType::Emoji;
+			result->isTextColor = items.front()->emojiUsesTextColor();
+			result->items = std::move(items);
+			return result;
+		}
+		return std::unique_ptr<WebPageStickerSet>();
+	}, [&](const auto &) {
+		return std::unique_ptr<WebPageStickerSet>();
+	});
+}
+
+[[nodiscard]] int ExtractDuration(const TLlinkPreviewType &type) {
+	return type.match([&](const TLDlinkPreviewTypeAnimation &data) {
+		return data.vanimation().data().vduration().v;
+	}, [&](const TLDlinkPreviewTypeAudio &data) {
+		return data.vaudio().data().vduration().v;
+	}, [&](const TLDlinkPreviewTypeEmbeddedVideoPlayer &data) {
+		return data.vduration().v;
+	}, [&](const TLDlinkPreviewTypeVideo &data) {
+		return data.vvideo().data().vduration().v;
+	}, [&](const TLDlinkPreviewTypeVideoNote &data) {
+		return data.vvideo_note().data().vduration().v;
+	}, [&](const TLDlinkPreviewTypeVoiceNote &data) {
+		return data.vvoice_note().data().vduration().v;
+	}, [&](const auto &) {
+		return 0;
+	});
+}
+
 } // namespace
 
+#if 0 // mtp
 WebPageType ParseWebPageType(
 		const QString &type,
 		const QString &embedUrl,
@@ -184,6 +545,7 @@ WebPageType ParseWebPageType(
 		return WebPageType::Article;
 	}
 }
+#endif
 
 bool IgnoreIv(WebPageType type) {
 	return !Iv::ShowButton()
@@ -191,6 +553,7 @@ bool IgnoreIv(WebPageType type) {
 		|| (type == WebPageType::Album);
 }
 
+#if 0 // mtp
 WebPageType ParseWebPageType(const MTPDwebPage &page) {
 	return ParseWebPageType(
 		qs(page.vtype().value_or_empty()),
@@ -198,7 +561,6 @@ WebPageType ParseWebPageType(const MTPDwebPage &page) {
 		!!page.vcached_page());
 }
 
-#if 0 // mtp
 WebPageCollage::WebPageCollage(
 	not_null<Data::Session*> owner,
 	const MTPDwebPage &data)
@@ -213,49 +575,59 @@ WebPageData::WebPageData(not_null<Data::Session*> owner, const WebPageId &id)
 
 WebPageData::~WebPageData() = default;
 
-WebPageId WebPageData::IdFromTdb(const TLwebPage &data) {
+WebPageId WebPageData::IdFromTdb(const TLlinkPreview &data) {
 	const auto &url = data.data().vurl().v;
 	return XXH64(url.data(), url.size() * sizeof(ushort), 0);
 }
 
-void WebPageData::setFromTdb(const TLwebPage &data) {
+void WebPageData::setFromTdb(const TLlinkPreview &data) {
 	const auto &fields = data.data();
 	applyChanges(
 		ParseWebPageType(
-			fields.vtype().v,
-			fields.vembed_url().v,
+			fields.vtype(),
 			fields.vinstant_view_version().v > 0),
 		fields.vurl().v,
 		fields.vdisplay_url().v,
 		fields.vsite_name().v,
 		fields.vtitle().v,
 		Api::FormattedTextFromTdb(fields.vdescription()),
-		FullStoryId{
-			peerFromTdbChat(fields.vstory_sender_chat_id()),
-			fields.vstory_id().v,
-		},
-		(fields.vphoto()
-			? _owner->processPhoto(*fields.vphoto()).get()
-			: nullptr),
-		(fields.vanimation()
-			? _owner->processDocument(*fields.vanimation()).get()
-			: fields.vaudio()
-			? _owner->processDocument(*fields.vaudio()).get()
-			: fields.vdocument()
-			? _owner->processDocument(*fields.vdocument()).get()
-			: fields.vsticker()
-			? _owner->processDocument(*fields.vsticker()).get()
-			: fields.vvideo()
-			? _owner->processDocument(*fields.vvideo()).get()
-			: fields.vvideo_note()
-			? _owner->processDocument(*fields.vvideo_note()).get()
-			: fields.vvoice_note()
-			? _owner->processDocument(*fields.vvoice_note()).get()
-			: nullptr),
-		WebPageCollage(), // todo
-		fields.vduration().v,
+		ExtractStoryId(fields.vtype()),
+		ExtractPhoto(_owner, fields.vtype()),
+		ExtractDocument(_owner, fields.vtype()),
+		ExtractStickerSet(_owner, fields.vtype()),
+		ExtractDuration(fields.vtype()),
 		fields.vauthor().v,
+		fields.vhas_large_media().v,
 		0);
+
+	if (fields.vinstant_view_version().v > 0 && !_collageRequestId) {
+		const auto ignore = IgnoreIv(type);
+		const auto owner = _owner;
+		const auto sender = &_owner->session().sender();
+		const auto requestId = sender->preallocateId();
+		const auto finish = [owner, id = id, requestId](
+				std::unique_ptr<Iv::Data> iv,
+				WebPageCollage &&collage) {
+			const auto that = owner->webpage(id);
+			if (that->_collageRequestId == requestId) {
+				that->_collageRequestId = -1;
+				that->setIv(std::move(iv), std::move(collage));
+			}
+		};
+		sender->request(TLgetWebPageInstantView(
+			fields.vurl(),
+			tl_bool(false) // force_full
+		), requestId).done([=](const TLwebPageInstantView &iv) {
+			finish(
+				(ignore
+					? nullptr
+					: std::make_unique<Iv::Data>(data.data().vurl().v, iv)),
+				ExtractCollage(owner, iv.data()));
+		}).fail([finish] {
+			finish(nullptr, {});
+		}).send();
+		_collageRequestId = requestId;
+	}
 }
 
 Data::Session &WebPageData::owner() const {
@@ -276,8 +648,10 @@ bool WebPageData::applyChanges(
 		FullStoryId newStoryId,
 		PhotoData *newPhoto,
 		DocumentData *newDocument,
+#if 0 // mtp
 		WebPageCollage &&newCollage,
 		std::unique_ptr<Iv::Data> newIv,
+#endif
 		std::unique_ptr<WebPageStickerSet> newStickerSet,
 		int newDuration,
 		const QString &newAuthor,
@@ -314,7 +688,9 @@ bool WebPageData::applyChanges(
 	const auto hasTitle = !resultTitle.isEmpty() ? 1 : 0;
 	const auto hasDescription = !newDescription.text.isEmpty() ? 1 : 0;
 	if (newDocument
+#if 0 // mtp
 		|| !newCollage.items.empty()
+#endif
 		|| !newPhoto
 		|| (hasSiteName + hasTitle + hasDescription < 2)) {
 		newHasLargeMedia = false;
@@ -329,9 +705,11 @@ bool WebPageData::applyChanges(
 		&& storyId == newStoryId
 		&& photo == newPhoto
 		&& document == newDocument
+#if 0 // mtp
 		&& collage.items == newCollage.items
 		&& (!iv == !newIv)
 		&& (!iv || iv->partial() == newIv->partial())
+#endif
 		&& (!stickerSet == !newStickerSet)
 		&& duration == newDuration
 		&& author == resultAuthor
@@ -354,8 +732,10 @@ bool WebPageData::applyChanges(
 	storyId = newStoryId;
 	photo = newPhoto;
 	document = newDocument;
+#if 0 // mtp
 	collage = std::move(newCollage);
 	iv = std::move(newIv);
+#endif
 	stickerSet = std::move(newStickerSet);
 	duration = newDuration;
 	author = resultAuthor;
@@ -374,6 +754,24 @@ bool WebPageData::applyChanges(
 void WebPageData::replaceDocumentGoodThumbnail() {
 	if (document && photo) {
 		document->setGoodThumbnailPhoto(photo);
+	}
+}
+
+void WebPageData::setIv(
+		std::unique_ptr<Iv::Data> newIv,
+		WebPageCollage &&newCollage) {
+	const auto was = version;
+	if ((!iv != !newIv)
+		|| (iv && iv->partial() != newIv->partial())) {
+		iv = std::move(newIv);
+		++version;
+	}
+	if (collage.items != newCollage.items) {
+		collage = std::move(newCollage);
+		++version;
+	}
+	if (version != was) {
+		_owner->notifyWebPageUpdateDelayed(this);
 	}
 }
 
