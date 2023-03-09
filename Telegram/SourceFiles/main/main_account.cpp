d126ae73e6a7284ec6ec7f269ab337c75413b465
John Preston <johnprestonmail@gmail.com>
2023-03-09 12:39:23 +0400

    Use common language_pack_database_path.

--- Telegram/SourceFiles/main/main_account.cpp
+++ Telegram/SourceFiles/main/main_account.cpp
@@ -116,6 +116,8 @@ void Account::start(std::unique_ptr<MTP::Config> config) {
 
 std::unique_ptr<Tdb::Account> Account::createTdb() {
 	const auto key = domain().tdbKey();
+	const auto langpackPath = cWorkingDir() + u"tdata/lang"_q;
+	QDir().mkpath(langpackPath);
 	auto result = std::make_unique<Tdb::Account>(Tdb::AccountConfig{
 		.apiId = ApiId,
 		.apiHash = ApiHash,
@@ -125,6 +127,7 @@ std::unique_ptr<Tdb::Account> Account::createTdb() {
 		.applicationVersion = QString::fromLatin1(AppVersionStr),
 		.databaseDirectory = _local->libDatabasePath(),
 		.filesDirectory = _local->libFilesPath(),
+		.langpackDirectory = langpackPath,
 		.encryptionKey = QByteArray(
 			reinterpret_cast<const char*>(key.data()),
 			key.size()),
