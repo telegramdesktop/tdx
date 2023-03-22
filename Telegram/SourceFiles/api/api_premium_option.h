0d20ffef7a520a45f7985b85c536c3444ece7d2f
John Preston <johnprestonmail@gmail.com>
2023-03-22 10:08:21 +0400

    Migrate support user info to TDLib.

--- Telegram/SourceFiles/api/api_premium_option.h
+++ Telegram/SourceFiles/api/api_premium_option.h
@@ -53,7 +53,7 @@ template<typename Option>
 			continue;
 		}
 #if 0 // mtp
-		const auto botUrl = !lnk // later internalLink types.
+		const auto botUrl = !lnk
 			? QString()
 			: (lnk->type() == Tdb::id_internalLinkTypeInvoice)
 			? ("https://t.me/$"
