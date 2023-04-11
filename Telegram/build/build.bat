30dedcdd0f0604952909c7ae6a30d2c4128140b1
John Preston <johnprestonmail@gmail.com>
2023-04-11 16:54:45 +0400

    [tdx] Don't build and deploy Windows setup.

--- Telegram/build/build.bat
+++ Telegram/build/build.bat
@@ -208,17 +208,17 @@ if %BuildUWP% equ 0 (
     goto sign2
   )
 
-  if %AlphaVersion% equ 0 (
-    iscc /dMyAppVersion=%AppVersionStrSmall% /dMyAppVersionZero=%AppVersionStr% /dMyAppVersionFull=%AppVersionStrFull% "/dReleasePath=%ReleasePath%" "/dMyBuildTarget=%BuildTarget%" "%FullScriptPath%setup.iss"
-    if %errorlevel% neq 0 goto error
-    if not exist "%SetupFile%" goto error
-:sign3
-    call "%SignPath%" "%SetupFile%"
-    if %errorlevel% neq 0 (
-      timeout /t 3
-      goto sign3
-    )
-  )
+rem  if %AlphaVersion% equ 0 (
+rem    iscc /dMyAppVersion=%AppVersionStrSmall% /dMyAppVersionZero=%AppVersionStr% /dMyAppVersionFull=%AppVersionStrFull% "/dReleasePath=%ReleasePath%" "/dMyBuildTarget=%BuildTarget%" "%FullScriptPath%setup.iss"
+rem    if %errorlevel% neq 0 goto error
+rem    if not exist "%SetupFile%" goto error
+rem:sign3
+rem    call "%SignPath%" "%SetupFile%"
+rem    if %errorlevel% neq 0 (
+rem      timeout /t 3
+rem      goto sign3
+rem    )
+rem  )
 
   call Packer.exe -version %VersionForPacker% -path %BinaryName%.exe -path Updater.exe -path "modules\%Platform%\d3d\d3dcompiler_47.dll" -target %BuildTarget% %AlphaBetaParam%
   if %errorlevel% neq 0 goto error
@@ -302,9 +302,10 @@ if %BuildUWP% neq 0 (
   move "%ReleasePath%\Updater.exe" "%DeployPath%\"
   move "%ReleasePath%\%BinaryName%.pdb" "%DeployPath%\"
   move "%ReleasePath%\Updater.pdb" "%DeployPath%\"
-  if %AlphaVersion% equ 0 (
-    move "%ReleasePath%\%SetupFile%" "%DeployPath%\"
-  ) else (
+rem  if %AlphaVersion% equ 0 (
+rem    move "%ReleasePath%\%SetupFile%" "%DeployPath%\"
+rem  ) else (
+  if %AlphaVersion% neq 0 (
     move "%ReleasePath%\%AlphaKeyFile%" "%DeployPath%\"
   )
   move "%ReleasePath%\%UpdateFile%" "%DeployPath%\"
@@ -332,9 +333,9 @@ if %BuildUWP% equ 0 (
 
   if not exist "%DeployPath%\%UpdateFile%" goto error
   if not exist "%DeployPath%\%PortableFile%" goto error
-  if %AlphaVersion% equ 0 (
-    if not exist "%DeployPath%\%SetupFile%" goto error
-  )
+rem  if %AlphaVersion% equ 0 (
+rem    if not exist "%DeployPath%\%SetupFile%" goto error
+rem  )
   if not exist "%DeployPath%\%BinaryName%.pdb" goto error
   if not exist "%DeployPath%\Updater.exe" goto error
   if not exist "%DeployPath%\Updater.pdb" goto error
@@ -342,9 +343,10 @@ if %BuildUWP% equ 0 (
 
   xcopy "%DeployPath%\%UpdateFile%" "%FinalDeployPath%\" /Y
   xcopy "%DeployPath%\%PortableFile%" "%FinalDeployPath%\" /Y
-  if %AlphaVersion% equ 0 (
-    xcopy "%DeployPath%\%SetupFile%" "%FinalDeployPath%\" /Y
-  ) else (
+rem  if %AlphaVersion% equ 0 (
+rem    xcopy "%DeployPath%\%SetupFile%" "%FinalDeployPath%\" /Y
+rem  ) else (
+  if %AlphaVersion% neq 0 (
     xcopy "%DeployPath%\%AlphaKeyFile%" "%FinalDeployPath%\" /Y
   )
 )
