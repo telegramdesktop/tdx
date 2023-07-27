a180ae70b7efd6321f1fde99696f7f82c0f52441
John Preston <johnprestonmail@gmail.com>
2023-07-27 22:28:29 +0400

    Fix login with a phone number.

--- Telegram/SourceFiles/intro/intro_step.cpp
+++ Telegram/SourceFiles/intro/intro_step.cpp
@@ -227,8 +227,10 @@ void Step::jumpByState(const TLauthorizationState &state) {
 	}, [](const TLDauthorizationStateLoggingOut &) {
 	}, [](const TLDauthorizationStateClosing &) {
 	}, [&](const TLDauthorizationStateClosed &) {
-		LOG(("Tdb Error: Got Closed state while authorizing."));
-		go(StepType::Qr);
+		if (!getData()->resettingForPhoneAuth) {
+			LOG(("Tdb Error: Got Closed state while authorizing."));
+			go(StepType::Qr);
+		}
 	}, [](const TLDauthorizationStateWaitEmailAddress &) {
 		LOG(("Tdb Error: Should not StateWaitEmailAddress in TDesktop."));
 	}, [](const TLDauthorizationStateWaitEmailCode &) {
