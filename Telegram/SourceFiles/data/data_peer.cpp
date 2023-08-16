b89634a828102875684e06aaa7243796cdd4ca91
John Preston <johnprestonmail@gmail.com>
2023-08-16 14:38:20 +0200

    Use canRevokeFullHistory from TDLib.

--- Telegram/SourceFiles/data/data_peer.cpp
+++ Telegram/SourceFiles/data/data_peer.cpp
@@ -1269,7 +1269,13 @@ bool PeerData::amAnonymous() const {
 			&& (asChannel()->adminRights() & ChatAdminRight::Anonymous));
 }
 
+void PeerData::setCanRevokeFullHistory(bool can) {
+	_canRevokeFullHistory = can;
+}
+
 bool PeerData::canRevokeFullHistory() const {
+	return _canRevokeFullHistory;
+#if 0 // mtp
 	if (const auto user = asUser()) {
 		return !isSelf()
 			&& (!user->isBot() || user->isSupport())
@@ -1283,6 +1289,7 @@ bool PeerData::canRevokeFullHistory() const {
 			&& megagroup->canDelete();
 	}
 	return false;
+#endif
 }
 
 bool PeerData::slowmodeApplied() const {
