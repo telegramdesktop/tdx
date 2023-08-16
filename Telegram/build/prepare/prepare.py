db672d3355d866b66aa66d4dea989476d2fdf7c6
John Preston <johnprestonmail@gmail.com>
2023-08-16 11:06:16 +0200

    Update TDLib to 1.8.16 and fix the build.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1904,7 +1904,7 @@ win:
 stage('td', """
     git clone https://github.com/tdlib/td.git
     cd td
-    git checkout 8893dc808f
+    git checkout cde095db6c
 win:
     SET OPENSSL_DIR=%LIBS_DIR%\\openssl
     SET OPENSSL_LIBS_DIR=%OPENSSL_DIR%\\out
