e9e72f8f9ebb88b1af64a2c046f77fd287c66be4
John Preston <johnprestonmail@gmail.com>
2023-09-15 15:48:17 +0400

    Update TDLib to 1.8.18 and fix the build.

--- Telegram/SourceFiles/api/api_updates.cpp
+++ Telegram/SourceFiles/api/api_updates.cpp
@@ -3081,6 +3081,8 @@ void Updates::applyUpdate(const TLupdate &update) {
 			data.vterms_of_service().data(),
 			data.vterms_of_service_id().v.toUtf8()));
 	}, [&](const TLDupdateUsersNearby &data) {
+	}, [&](const TLDupdateUnconfirmedSession &data) {
+		// later
 	}, [&](const TLDupdateAttachmentMenuBots &data) {
 		session().attachWebView().apply(data);
 	}, [&](const TLDupdateWebAppMessageSent &data) {
