6e663f849c339aa83e071f26b755eb9e8052b1a7
23rd <23rd@vivaldi.net>
2023-11-17 19:12:13 +0300

    Migrated API for channel statistics from MTP to TDLib.

--- Telegram/SourceFiles/api/api_statistics_data_deserialize.h
+++ Telegram/SourceFiles/api/api_statistics_data_deserialize.h
@@ -7,13 +7,21 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
+namespace Tdb {
+class TLstatisticalGraph;
+} // namespace Tdb
+
 namespace Data {
 struct StatisticalGraph;
 } // namespace Data
 
 namespace Api {
 
+#if 0 // mtp
 [[nodiscard]] Data::StatisticalGraph StatisticalGraphFromTL(
 	const MTPStatsGraph &tl);
+#endif
+[[nodiscard]] Data::StatisticalGraph StatisticalGraphFromTL(
+	const Tdb::TLstatisticalGraph &tl);
 
 } // namespace Api
