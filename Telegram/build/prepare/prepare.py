a63b4c576b481feb02625d2f8e836a6d0252dff2
John Preston <johnprestonmail@gmail.com>
2024-04-10 13:44:04 +0400

    Update TDLib code to 1.8.27. Build broken.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1687,9 +1687,9 @@ win:
 stage('td', """
     git clone https://github.com/tdlib/td.git
     cd td
-    git checkout ec788c7505
+    git checkout d7203eb719
 win:
-    SET OPENSSL_DIR=%LIBS_DIR%\\openssl
+    SET OPENSSL_DIR=%LIBS_DIR%\\openssl3
     SET OPENSSL_LIBS_DIR=%OPENSSL_DIR%\\out
     SET ZLIB_DIR=%LIBS_DIR%\\zlib
     SET ZLIB_LIBS_DIR=%LIBS_DIR%\\zlib\\contrib\\vstudio\\vc14\\%X8664%
@@ -1699,16 +1699,16 @@ win:
     cd Debug
     cmake -A %WIN32X64% ^
         -DOPENSSL_FOUND=1 ^
-        -DOPENSSL_INCLUDE_DIR=%OPENSSL_DIR%\include ^
+        -DOPENSSL_INCLUDE_DIR=%OPENSSL_DIR%\\include ^
         -DOPENSSL_CRYPTO_LIBRARY="%OPENSSL_LIBS_DIR%.dbg\libcrypto.lib" ^
         -DGPERF_EXECUTABLE=%ROOT_DIR%\\ThirdParty\\gperf\\bin\\gperf ^
         -DZLIB_FOUND=1 ^
         -DZLIB_INCLUDE_DIR=%ZLIB_DIR% ^
-        -DZLIB_LIBRARIES="%ZLIB_LIBS_DIR%\ZlibStatDebug\zlibstat.lib" ^
+        -DZLIB_LIBRARIES="%ZLIB_LIBS_DIR%\\ZlibStatDebug\\zlibstat.lib" ^
         -DCMAKE_CXX_FLAGS_DEBUG="/DZLIB_WINAPI /DNDEBUG /MTd /Zi /Od /Ob0" ^
         -DCMAKE_C_FLAGS_DEBUG="/DNDEBUG /MTd /Zi /Od /Ob0" ^
-        -DCMAKE_EXE_LINKER_FLAGS="/SAFESEH:NO Ws2_32.lib Gdi32.lib Advapi32.lib Crypt32.lib User32.lib %OPENSSL_LIBS_DIR%.dbg\libssl.lib" ^
-        -DCMAKE_SHARED_LINKER_FLAGS="/SAFESEH:NO Ws2_32.lib Gdi32.lib Advapi32.lib Crypt32.lib User32.lib %OPENSSL_LIBS_DIR%.dbg\libssl.lib" ^
+        -DCMAKE_EXE_LINKER_FLAGS="/SAFESEH:NO Ws2_32.lib Gdi32.lib Advapi32.lib Crypt32.lib User32.lib %OPENSSL_LIBS_DIR%.dbg\\libssl.lib" ^
+        -DCMAKE_SHARED_LINKER_FLAGS="/SAFESEH:NO Ws2_32.lib Gdi32.lib Advapi32.lib Crypt32.lib User32.lib %OPENSSL_LIBS_DIR%.dbg\\libssl.lib" ^
         -DTD_ENABLE_MULTI_PROCESSOR_COMPILATION=ON ^
         ../..
     cmake --build . --config Debug --target tdclient
@@ -1718,16 +1718,16 @@ release:
     cd Release
     cmake -A %WIN32X64% ^
         -DOPENSSL_FOUND=1 ^
-        -DOPENSSL_INCLUDE_DIR=%OPENSSL_DIR%\include ^
+        -DOPENSSL_INCLUDE_DIR=%OPENSSL_DIR%\\include ^
         -DOPENSSL_CRYPTO_LIBRARY="%OPENSSL_LIBS_DIR%\libcrypto.lib" ^
         -DGPERF_EXECUTABLE=%ROOT_DIR%\\ThirdParty\\gperf\\bin\\gperf ^
         -DZLIB_FOUND=1 ^
         -DZLIB_INCLUDE_DIR=%ZLIB_DIR% ^
-        -DZLIB_LIBRARIES="%ZLIB_LIBS_DIR%\ZlibStatDebug\zlibstat.lib" ^
+        -DZLIB_LIBRARIES="%ZLIB_LIBS_DIR%\\ZlibStatDebug\\zlibstat.lib" ^
         -DCMAKE_CXX_FLAGS_RELEASE="/DZLIB_WINAPI /MT /Ob2" ^
         -DCMAKE_C_FLAGS_RELEASE="/MT /Ob2" ^
-        -DCMAKE_EXE_LINKER_FLAGS="/SAFESEH:NO Ws2_32.lib Gdi32.lib Advapi32.lib Crypt32.lib User32.lib %OPENSSL_LIBS_DIR%\libssl.lib" ^
-        -DCMAKE_SHARED_LINKER_FLAGS="/SAFESEH:NO Ws2_32.lib Gdi32.lib Advapi32.lib Crypt32.lib User32.lib %OPENSSL_LIBS_DIR%\libssl.lib" ^
+        -DCMAKE_EXE_LINKER_FLAGS="/SAFESEH:NO Ws2_32.lib Gdi32.lib Advapi32.lib Crypt32.lib User32.lib %OPENSSL_LIBS_DIR%\\libssl.lib" ^
+        -DCMAKE_SHARED_LINKER_FLAGS="/SAFESEH:NO Ws2_32.lib Gdi32.lib Advapi32.lib Crypt32.lib User32.lib %OPENSSL_LIBS_DIR%\\libssl.lib" ^
         -DTD_ENABLE_MULTI_PROCESSOR_COMPILATION=ON ^
         ../..
     cmake --build . --config Release --target tdclient
@@ -1740,9 +1740,9 @@ mac:
             -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
             -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" \
             -DOPENSSL_FOUND=1 \
-            -DOPENSSL_INCLUDE_DIR=$LIBS_DIR/openssl/include \
-            -DOPENSSL_SSL_LIBRARY=$LIBS_DIR/openssl/libssl.a \
-            -DOPENSSL_CRYPTO_LIBRARY=$LIBS_DIR/openssl/libcrypto.a \
+            -DOPENSSL_INCLUDE_DIR=$LIBS_DIR/openssl3/include \
+            -DOPENSSL_SSL_LIBRARY=$LIBS_DIR/openssl3/libssl.a \
+            -DOPENSSL_CRYPTO_LIBRARY=$LIBS_DIR/openssl3/libcrypto.a \
             -DZLIB_FOUND=1 \
             -DZLIB_LIBRARIES=$USED_PREFIX/lib/libz.a \
             ../..
